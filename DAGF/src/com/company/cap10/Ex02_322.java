b8294b502660e96550f01e03503a46e5d039475f
walescko <walescko@gmail.com>
2021-12-15 07:11:14 -0300

    Imprimindo as matrizes na tela

--- DAGF/src/com/company/cap10/Ex02_322.java
+++ DAGF/src/com/company/cap10/Ex02_322.java
@@ -17,7 +17,7 @@ public class Ex02_322 {
 
         System.out.print("Qual a quantidade de linhas das matrizes? ");
         n = scan.nextInt();
-        System.out.print("Qual a quantidade de colunas das matrizes");
+        System.out.print("Qual a quantidade de colunas das matrizes? ");
         m = scan.nextInt();
         System.out.println("\nAs matrizes terão " + n + " linhas e " + m + " colunas.");
 
@@ -27,10 +27,21 @@ public class Ex02_322 {
             System.out.println("Entre com os valores da Matriz " + k+1);
             for (int i = 0; i < n; i++){
                 for (int j = 0; j < m; j++){
-                    System.out.print("Entre com o elemento " + (i+1) + (j+1));
+                    System.out.print("Entre com o elemento " + (i+1) + (j+1) + ": ");
                     matriz[k][i][j] = scan.nextInt();
                 }
             }
         }
+
+        for (k = 0 ; k < 2 ; k++){
+            System.out.println("Matriz " + (k+1) + ":");
+                for (int i = 0; i < n; i++){
+                    for (int j = 0; j < m ; j++){
+                        System.out.print(matriz[k][i][j] + " ");
+                    }
+                    System.out.println();
+                }
+        }
     }
 }
+
