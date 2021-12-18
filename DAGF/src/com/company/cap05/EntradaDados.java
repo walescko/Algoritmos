a6b0df7f22c617fd39b38df504b2ac3d98a6600b
walescko <walescko@gmail.com>
2021-12-17 22:28:48 -0300

    Exemplo de entrada de dados

---
+++ DAGF/src/com/company/cap05/EntradaDados.java
@@ -0,0 +1,15 @@
+package com.company.cap05;
+
+import java.util.Scanner;
+
+public class EntradaDados {
+
+    public static void main(String[] args){
+
+        int numeroInteiro;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um numero inteiro: ");
+        numeroInteiro = scan.nextInt();
+        System.out.println("\nO numero digitado eh: " + numeroInteiro);
+    }
+}
