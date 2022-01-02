3e1e4e94fb034acbf3a54c31694169768210a22a
walescko <walescko@gmail.com>
2022-01-02 19:03:57 -0300

    Exemplo do capítulo 6

---
+++ DAGF/src/com/company/walescko/cap06/MaiorMenor.java
@@ -0,0 +1,31 @@
+package com.company.walescko.cap06;
+
+import java.util.Scanner;
+
+public class MaiorMenor {
+
+    public static void main(String[] args){
+
+        int numero1, numero2, maior, menor;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite dois números inteiros:");
+        System.out.print("A: ");
+        numero1 = scan.nextInt();
+        System.out.print("B: ");
+        numero2 = scan.nextInt();
+
+        if(numero1 > numero2){
+            maior = numero1;
+            menor = numero2;
+        } else {
+            maior = numero2;
+            menor = numero1;
+        }
+
+        System.out.println("Números lidos: " + numero1 + " e " + numero2);
+        System.out.println("Maior = " + maior);
+        System.out.println("Menor = " + menor);
+    }
+}
