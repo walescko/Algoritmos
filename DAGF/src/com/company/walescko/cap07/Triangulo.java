336cb1da32b90adc37c6c3b3a7c243de886a8630
walescko <walescko@gmail.com>
2022-01-02 19:14:18 -0300

    Exemplo cap07 - termiando

---
+++ DAGF/src/com/company/walescko/cap07/Triangulo.java
@@ -0,0 +1,24 @@
+package com.company.walescko.cap07;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+    public static void main(String[] args){
+
+        int ladoA, ladoB, ladoC;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor dos lados do triangulo:");
+        System.out.print("Lado A: ");
+        ladoA = scan.nextInt();
+        System.out.print("Lado B: ");
+        ladoB = scan.nextInt();
+        System.out.print("Lado C: ");
+        ladoC = scan.nextInt();
+
+        
+
+    }
+}
