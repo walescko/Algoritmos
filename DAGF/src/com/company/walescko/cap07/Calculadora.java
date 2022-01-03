146e1d51326154b79fe649b8426bb3295fdcffc8
walescko <walescko@gmail.com>
2022-01-02 21:03:49 -0300

    Exemplo switch Cap07

---
+++ DAGF/src/com/company/walescko/cap07/Calculadora.java
@@ -0,0 +1,40 @@
+package com.company.walescko.cap07;
+
+import java.util.Scanner;
+
+public class Calculadora {//Calculadora outra vez... em java...
+
+    public static void main(String[] arg){
+
+        Scanner scan = new Scanner(System.in);
+
+        int numeroA, numeroB;
+        char opcao;
+        String escolha, saida ="";
+        
+        System.out.println("Digite dois numeros e a operação (+, -, * , /):");
+        System.out.print("Numero A: ");
+        numeroA = scan.nextInt();
+        System.out.print("Numero B: ");
+        numeroB = scan.nextInt();
+        System.out.print("Escolha a operacao: ");
+        escolha = scan.next();
+        opcao = escolha.charAt(0);
+
+        switch(opcao){
+            case '+': saida += + numeroA + " + " + numeroB + " = " + (numeroA + numeroB); break;
+            case '-': saida += + numeroA + " - " + numeroB + " = " + (numeroA - numeroB); break;
+            case '*': saida += + numeroA + " * " + numeroB + " = " + (numeroA * numeroB); break;
+            case '/': {
+                if (numeroB == 0) {
+                    saida = "A operacao nao pode ser realizada";
+                } else {
+                    saida += +numeroA + " / " + numeroB + " = " + (numeroA / numeroB);
+                }
+                break;
+            }
+            default: saida = "Operacao Invalida";
+        }
+        System.out.println(saida);
+    }
+}
