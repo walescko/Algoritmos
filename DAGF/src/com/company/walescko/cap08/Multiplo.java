05efa129d81d1579ac804925ab19ded3d0c213e4
walescko <walescko@gmail.com>
2022-01-21 13:50:32 -0300

    exemplo cap[A - multiplo

---
+++ DAGF/src/com/company/walescko/cap08/Multiplo.java
@@ -0,0 +1,15 @@
+package com.company.walescko.cap08;
+
+import java.util.Scanner;
+
+public class Multiplo {
+    public static void main(String[] args){
+        int numero, soma, multiplo, i;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        numero = scan.nextInt();
+        
+    }
+}
