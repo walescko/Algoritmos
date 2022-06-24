d13670a22d0a2ba67e7d4871c20b5b49384c5523
Walescko <walescko@gmail.com>
2022-06-24 06:36:54 -0300

    C - enfim consegui ler string em C

--- DAGF-AlgortimoProgramacaoDeComputadores-exercicios/cap10/string-teste.c
+++ DAGF-AlgortimoProgramacaoDeComputadores-exercicios/cap10/string-teste.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include <stdio_ext.h>
 
-int main(){
+int main(void){
     char vet[10];
-    int i;
 
     scanf("%s",vet);
     __fpurge(stdin);
 
     printf("\nvet=: %s", vet);
 
+    return 0;
 }
